8852aa9786806577250eef842995cbd4218ff68b
Kishanveer Singh <kishanveersingh2704@gmail.com>
2026-04-12 15:18:51 +0100

    langgraph for agent state memory

--- OSINT.py
+++ OSINT.py
@@ -1,7 +1,8 @@
 import asyncio
-
 from playwright.async_api import async_playwright
 from playwright_stealth import Stealth
+from typing import TypedDict, List
+from langgraph.graph import StateGraph, END
 
 async def scrape_deep_content(url):
     async with async_playwright() as p:
@@ -17,7 +18,7 @@ async def scrape_deep_content(url):
         
         try:
             print(f"Scraping: {url}")
-            await page.goto(url, wait_until="networkidle", timeout=60000)
+            await page.goto(url, wait_until="networkidle", timeout=80000)
             
             # Extract the inner text of the body - this removes tags and keeps content
             content = await page.evaluate("() => document.body.innerText")
@@ -31,6 +32,106 @@ async def scrape_deep_content(url):
         finally:
             await browser.close()
 
+# Agent State (Agent's Memory)
+class ReseacherState(TypedDict):
+    objective: str
+    search_queries: List[str]
+    visited_url: List[str]
+    scraped_data: str
+    final_report: str
+
+# Nodes (The "Actors")
+def planner_node(state: ReseacherState):
+    print("\n-- [NODE: PLANNER] Generating Queries --")
+    # TODO: Add LLM prompt to read objective and the generate Google/Tavily queries
+    mock_queries = ["defense contractor acquisition 2026", "aerospace drone OSINT."]
+    return {"search_queries": mock_queries}
+
+
+def search_scraper_node(state: ReseacherState):
+    print("\n-- [NODE: SEARCH & SCRAPE Gathering data")
+    # TODO: Integrate Tavily search and the async Playwright script
+    target_url = "https://ai.plainenglish.io/train-test-split-explained-why-data-leakage-happens-so-easily-f2ad54bbfa08"
+    
+    print(f'Executing scraper on: {target_url}')
+    new_data = asyncio.run(scrape_deep_content(target_url))
+    
+    # Append to existing data
+    current_data = state.get("scraped_data", "")
+    updated_data = current_data + "\n\n-- NEW SOURCE --" + new_data
+    
+    # Update the visited urls
+    current_urls = state.get("visited_url", [])
+    current_urls.append(target_url)
+    
+    return {"scraped_data": updated_data, "visited_url": current_urls}
+        
+def evaluator_node(state: ReseacherState):
+    print("\n-- [NODE: EVALUATOR] Analyzing Gaps --")
+    # TODO: Add LLM prompt to chenck if 'scraped_data' satisfies the 'objective'
+    return state
+
+def reported_node(state: ReseacherState):
+    print("\n-- [NODE: REPORTER] Compiling Final Dossier --")
+    # TODO: Add LLM prompt to format the raw data into a clean Markdown report
+    dossier = "# Final OSINT Report\n\nAll objective met."
+    return {"final_report": dossier}
+
+# Routing Logic (The "Brain")
+def should_continue(state: ReseacherState):
+    print("\n-- [ROUTER] Deciding next steps --")
+    # TODO: This will eventually read the output of the Evaluator LLM.
+    
+    decision = "finish"
+    
+    if decision == "continue":
+        print("-> Missing Information. Looping back to planner.")
+        return "continue"
+    else:
+        print("-> Objectives met. Routing to Reporter.")
+        return "finish"
+
+# Build and compile the Graph
+workflow = StateGraph(ReseacherState)
+
+# Register Nodes
+workflow.add_node("planner", planner_node)
+workflow.add_node("search_scraper", search_scraper_node)
+workflow.add_node("evaluator", evaluator_node)
+workflow.add_node("reporter", reported_node)
+
+# Standard Linear Edges
+workflow.set_entry_point("planner")
+workflow.add_edge("planner", "search_scraper")
+workflow.add_edge("search_scraper", "evaluator")
+
+# Conditional edge (The loop)
+workflow.add_conditional_edges(
+    "evaluator",
+    should_continue,
+    {
+        "continue": "planner", # Loop back to start
+        "finish": "reporter" # Go to final step
+    }
+)
+
+# End the graph after reporting
+workflow.add_edge("reporter", END)
+
+# Compile into an executable application
+app = workflow.compile()
+
 if __name__ == "__main__":
-    scraped_text = asyncio.run(scrape_deep_content("https://ai.plainenglish.io/train-test-split-explained-why-data-leakage-happens-so-easily-f2ad54bbfa08"))
-    print(scraped_text)
+    initial_state = {
+        "objective": "Identify the primary capabilities od a specific defense AI framework.",
+        "search_queries": [],
+        "visited_urls": [],
+        "scraped_data": "",
+        "final_report": ""
+    }
+    
+    print("Starting Agentic Loop...")
+    # Invoke Graph
+    final_state = app.invoke(initial_state)
+    print("\n-- FINAL REPORT --")
+    print(final_state["final_report"])
